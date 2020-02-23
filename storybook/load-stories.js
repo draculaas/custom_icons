fc100cb66ff73fb4f22beb352fc810d950eb870a
denisschmidt <parasolgroup25@gmail.com>
2020-02-23 19:30:11 +0300

    update

--- storybook/load-stories.js
+++ storybook/load-stories.js
@@ -3,12 +3,12 @@ import { storiesOf } from '@storybook/react';
 
 import scenarios from '../src/**/*.scenario.js';
 
-const light = storiesOf('nexign-ui', module);
+const nexign = storiesOf('nexign-ui', module);
 
 scenarios.forEach(scenario => {
   const Component = scenario.result.default;
   const fileName = scenario.fileName.split('/').pop();
   const [scenarioName] = fileName.split('.scenario.js');
 
-  light.add(scenarioName, () => <Component />);
+  nexign.add(scenarioName, () => <Component />);
 });
